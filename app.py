dce37da227379fe4988cac232d7f4104541d829d
Sangopaa <146230386+Sangopaa@users.noreply.github.com>
2025-04-10 14:41:42 -0500

    Implement SQLALchemy and Alembic to have management version in db (#2)

--- app.py
+++ app.py
@@ -4,11 +4,24 @@ from flask import Flask
 from flask_cors import CORS
 from url import add_urls
 
+from sqlalchemy import NullPool
+from models import db
+
 config = Config()
 
+db_name = config.get("MYSQL_DATABASE_DB")
+connection_string = f"mysql+pymysql://{config.get('MYSQL_DATABASE_USER')}:{config.get('MYSQL_DATABASE_PASSWORD')}@{config.get('MYSQL_DATABASE_HOST')}:{config.get('MYSQL_DATABASE_PORT')}/{db_name}"
 
-def create_flask_app():
+
+def create_flask_app(use_null_pool=False):
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = connection_string
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = (
+        {"poolclass": NullPool} if use_null_pool else {"pool_recycle": 3600}
+    )
+
+    db.init_app(app)
+
     app_context = app.app_context()
     app_context.push()
     add_urls(app)
--- views/ping.py
+++ views/ping.py
@@ -1,10 +1,6 @@
 from flask_restful import Resource
-from config import Config
 
 
 class PingView(Resource):
     def get(self):
-        config_instance = Config()
-        secret_key = config_instance.get("SECRET_KEY")
-        print(secret_key)
         return {"ping": "pong"}, 200
